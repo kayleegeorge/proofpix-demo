4184252697893c803e9747e053b004d6f5d65da2
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2024-05-19 17:16:42 +0900

    add more routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rocket::serde::{json::Json, Deserialize};
+
 pub mod utils;
 
 #[macro_use]
@@ -14,7 +16,46 @@ fn challenge() -> String {
     return utils::generate_random_challenge();
 }
 
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct AttestationData<'r> {
+    attestation_string: &'r str,
+    raw_key_id: &'r str,
+}
+
+#[post("/appattest", format = "application/json", data = "<attestation_data>")]
+fn appattest(attestation_data: Json<AttestationData<'_>>) -> () {
+    const APP_ID: &str = "proof-pix";
+    const challenge: &str = "get-challenge"; // TODO: get challenge from db.
+
+    let verified = app_attest::validate_raw_attestation(
+        attestation_data.attestation_string,
+        challenge,
+        attestation_data.raw_key_id,
+        APP_ID,
+        false, // production
+        false, // leaf_cert_only
+    );
+
+    // TODO: do something with attestation data
+    if verified {
+        println!("Verified attestation");
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct ImageData {
+    image_data: Vec<u8>,
+    signature: Vec<u8>,
+}
+
+#[post("/image", format = "application/json", data = "<image_data>")]
+fn post_image(image_data: Json<ImageData>) -> () {
+    // TODO: do something with image data
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, challenge])
+    rocket::build().mount("/", routes![index, challenge, appattest, post_image])
 }
